71ba901be534dd3eb3d26e7260e8f19acad478ca
dlgeraghty <dvd.loge@gmail.com>
2022-05-01 12:20:13 +0100

    GFG delimiter

--- src/users.php
+++ src/users.php
@@ -1,10 +1,13 @@
 <?php
 
 echo "<h1> Create user </h1>"
-echo " <form method="POST" action="create-user.php">
+echo <<<GFG
+            <form method="POST" action="create-user.php">
                 <label for="username"> Username </label><br>
                 <input type="text" name="username">
                 <label for="age"> Age </label><br>
                 <input type="number" name="age">
             </form>"
+GFG;
+
 ?>
