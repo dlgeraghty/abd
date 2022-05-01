bf5d5947d44d238e9162d4757e9bf5725926db8c
dlgeraghty <dvd.loge@gmail.com>
2022-05-01 12:27:28 +0100

    create user backend

---
+++ src/create-user.php
@@ -0,0 +1,9 @@
+<?php
+
+$username = $_POST["username"];
+$age = $_POST["age"];
+
+printf("The username is: %s, and the age: %i\n", $username, $age);
+
+
+?>
--- src/users.php
+++ src/users.php
@@ -4,10 +4,11 @@ echo "<h1> Create user </h1>";
 echo <<<GFG
             <form method="POST" action="create-user.php">
                 <label for="username"> Username </label><br>
-                <input type="text" name="username">
+                <input type="text" name="username"><br>
                 <label for="age"> Age </label><br>
-                <input type="number" name="age">
-            </form>"
+                <input type="number" name="age"><br>
+                <input type="submit" value="Submit">
+            </form>
 GFG;
 
 ?>
