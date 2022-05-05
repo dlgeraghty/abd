b4c0d844191723a518f1e956eac5386aa9be5f7b
dlgeraghty <dvd.loge@gmail.com>
2022-05-05 19:53:08 +0100

    __DIR__

--- src/blog/list-blogs.php
+++ src/blog/list-blogs.php
@@ -1,7 +1,7 @@
 <?php
 
-include_once("../header.php");
-include_once("../config.php");
+include_once(__DIR__."../header.php");
+include_once(__DIR__."../config.php");
 
 $cursor = $collection->UserPosts->find();
 echo <<<GFG
