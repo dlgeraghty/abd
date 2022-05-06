751b5987e3d8d5a92b50d78ea9077fb3e92ce522
dlgeraghty <dvd.loge@gmail.com>
2022-05-06 11:15:59 +0100

    improve html

--- src/blog/list-blogs.php
+++ src/blog/list-blogs.php
@@ -5,6 +5,8 @@ include_once(__DIR__."/../config.php");
 
 $cursor = $collection->UserPosts->find();
 
+echo "<div class='container'>"
+
 foreach($cursor as $document){
 echo '<div class="card" style="width: 18rem;">';
 echo '<div class="card-body">';
@@ -19,4 +21,5 @@ echo '</div>';
 echo '</div>';
 }
 
+echo '</div>';
 ?>
