c0e5bdfd488482c854428eca6b9411b3371e21d4
dlgeraghty <dvd.loge@gmail.com>
2022-05-06 10:29:27 +0100

    incorporate bootstrap

--- src/nav.php
+++ src/nav.php
@@ -1,16 +1,40 @@
 <?php
 
 echo <<<GFG
-<ul>
-  <li><a href="index.php">Home</a></li>
-  <li><a href="users.php">Users</a></li>
-  <li><a href="blog.php">Blog</a></li>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title> ABD PROJECT </title>
+  </head>
+<body>
+
+<nav class="navbar navbar-dark">
+<div class="container-fluid">
+  <div class="navbar-header">
+      <a class="navbar-brand" href="#">Proyecto ABD J.David</a>
+  </div>
+  <ul class="nav navbar-nav">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="users.php">Users</a></li>
+    <li><a href="blog.php">Blog</a></li>
 GFG;
   if ($_SESSION['login']){
     echo "<li> Welcome, ". $_SESSION['username'] . "</li>";
-  }else{
-    echo "session username: " . $_SESSION['username'] . " session login:" . $_SESSION['login'];
   }
-echo "</ul>";
+echo <<< GFG
+    </ul>
+  </div>
+</nav>
+GFG;
+
+
+GFG;
 
 ?>
