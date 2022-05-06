7afbeaefa3397549aabd627c340914669a2f5b8b
dlgeraghty <dvd.loge@gmail.com>
2022-05-06 11:13:37 +0100

    separate cards

--- src/blog/list-blogs.php
+++ src/blog/list-blogs.php
@@ -4,12 +4,10 @@ include_once(__DIR__."/../header.php");
 include_once(__DIR__."/../config.php");
 
 $cursor = $collection->UserPosts->find();
-echo <<<GFG
-<div class="card" style="width: 18rem;">
-<div class="card-body">
-GFG;
 
 foreach($cursor as $document){
+echo '<div class="card" style="width: 18rem;">';
+echo '<div class="card-body">';
     echo "<h5 class='card-title'>".$document['title']."</h5>";
     echo "<h6 class='card-subtitle mb-2 text-muted'>".$document['categoria']."</h6>";
     echo "<p class='card-text'>".$document['content']."</p>";
@@ -17,10 +15,8 @@ foreach($cursor as $document){
         echo "<a href='#' class='card-link'>edit</a>";
     }
     echo "<a href='#' class='card-link'>".$document['creator']."</a>";
+echo '</div>';
+echo '</div>';
 }
-echo <<<GFG
-    </div>
-</div>
-GFG;
 
 ?>
