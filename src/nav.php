b7f700f7998b64da05746cfdd6ac9e06c0c7a1b1
dlgeraghty <dvd.loge@gmail.com>
2022-05-06 10:53:47 +0100

    logout

--- src/nav.php
+++ src/nav.php
@@ -22,6 +22,8 @@ echo <<<GFG
 GFG;
   if ($_SESSION['login']){
     echo "<li class='nav-item'><a class='nav-link' href='#'> Welcome, ". $_SESSION['username'] . "</a></li>";
+    echo "<li class='nav-item'><a class='nav-link' href='users/user-logout.php'> Welcome, ". $_SESSION['username'] . "</a></li>";
+
   }
 echo <<<GFG
 </ul>
