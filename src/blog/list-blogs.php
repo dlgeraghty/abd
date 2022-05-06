3ef0953b0f6187409378c4abd61c74f27db7cd73
dlgeraghty <dvd.loge@gmail.com>
2022-05-06 11:21:03 +0100

    grid for card blogs

--- src/blog/list-blogs.php
+++ src/blog/list-blogs.php
@@ -6,20 +6,25 @@ include_once(__DIR__."/../config.php");
 $cursor = $collection->UserPosts->find();
 
 echo "<div class='container'>";
+echo '<div class="row">';
 
 foreach($cursor as $document){
-echo '<div class="card" style="width: 18rem;">';
-echo '<div class="card-body">';
-    echo "<h5 class='card-title'>".$document['title']."</h5>";
-    echo "<h6 class='card-subtitle mb-2 text-muted'>".$document['categoria']."</h6>";
-    echo "<p class='card-text'>".$document['content']."</p>";
-    if($_SESSION['login'] && $_SESSION['username'] == $document['creator']){
-        echo "<a href='#' class='card-link'>edit</a>";
-    }
-    echo "<a href='#' class='card-link'>".$document['creator']."</a>";
-echo '</div>';
+
+echo '<div class="col-sm-4">';
+    echo '<div class="card" style="width: 18rem;">';
+        echo '<div class="card-body">';
+            echo "<h5 class='card-title'>".$document['title']."</h5>";
+            echo "<h6 class='card-subtitle mb-2 text-muted'>".$document['categoria']."</h6>";
+            echo "<p class='card-text'>".$document['content']."</p>";
+            if($_SESSION['login'] && $_SESSION['username'] == $document['creator']){
+                echo "<a href='#' class='card-link'>edit</a>";
+            }
+            echo "<a href='#' class='card-link'>".$document['creator']."</a>";
+        echo '</div>';
+    echo '</div>';
 echo '</div>';
 }
 
+echo '</div>';
 echo '</div>';
 ?>
