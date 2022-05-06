95f812395b9c03dd5ffa0db9ae23bdc5121bcb20
dlgeraghty <dvd.loge@gmail.com>
2022-05-06 10:48:02 +0100

    simplify

--- src/nav.php
+++ src/nav.php
@@ -11,26 +11,20 @@ echo <<<GFG
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
     <title> ABD PROJECT </title>
   </head>
 <body>
 
-<nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="#">Proyecto ABD J.David</a>
-  <div class="collapse navbar-collapse">
-    <a class="nav-item" href="index.php">Home</a>
-    <a class="nav-item" href="users.php">Users</a>
-    <a class="nav-item" href="blog.php">Blog</a>
+<ul class="nav">
+  <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+  <li class="nav-item"><a class="nav-link" href="users.php">Users</a></li>
+  <li class="nav-item"><a class="nav-link" href="blog.php">Blog</a></li>
 GFG;
   if ($_SESSION['login']){
-    echo "<a class='nav-item'> Welcome, ". $_SESSION['username'] . "</a>";
+    echo "<li class='nav-item'> Welcome, ". $_SESSION['username'] . "</li>";
   }
 echo <<<GFG
-  </div>
-</nav>
+</ul>
 GFG;
 
 
